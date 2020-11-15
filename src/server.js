d0f6201a603088e9bd1125a2ee6ec37d716cb4f0
Pablo Mestanza <pablo93ml@gmail.com>
2020-11-15 11:57:23 +0100

    feat: routes file

---
+++ src/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const constants = require('./constants');
+
+const router = express.Router();
+
+
+router.get('/', (req, res) => {
+    res.sendFile(path.join(constants.VIEWS_PATH + 'home.html'));
+});
+
+router.get('/services', (req, res) => {
+    res.sendFile(path.join(constants.VIEWS_PATH + 'services.html'));
+});
+
+router.get('/contact', (req, res) => {
+    res.sendFile(path.join(constants.VIEWS_PATH + 'contact.html'));
+});
+
+
+
+app.use('/', router);
+
+app.listen(constants.PORT, () => {
+    console.log(`Server listening on port ${constants.PORT}`);
+});
+
+
+
+
